24ff5fa58f6dbaacba4a3b7035c073c9d4a517ee
Vinícius <142949993+ViniGBPl@users.noreply.github.com>
2025-02-24 02:00:01 -0300

    adicionado o trigger para quantidade de funcionários

--- Trigger_supermercado.sql
+++ Trigger_supermercado.sql
@@ -7,6 +7,8 @@ add column idade int;
 
 #trigger para calcular a idade do cliente durante o cadastro 
 
+
+
 DELIMITER //
 
 CREATE TRIGGER calcular_idade_insert
@@ -17,4 +19,104 @@ BEGIN
 END //
 
 DELIMITER ;
+
+####falta testar
+#Trigger para atualizar a quantidade de produto
+
+#Trigger para adionar uma quantidade de produto
+
+DELIMITER //
+
+CREATE TRIGGER atualizar_estoque_insert
+AFTER INSERT ON ITEM_ESTOQUE
+FOR EACH ROW
+BEGIN
+    UPDATE PRODUTO_REF
+    SET qtd_estoque = qtd_estoque + NEW.qtd_atual
+    WHERE Cod = NEW.Cod_Prod;
+END //
+
+DELIMITER ;
+
+DELIMITER ;
+
+
+#Trigger para atualizar a quantidade de produto 
+DELIMITER //
+
+CREATE TRIGGER atualizar_estoque_update
+AFTER UPDATE ON ITEM_ESTOQUE
+FOR EACH ROW
+BEGIN
+    UPDATE PRODUTO_REF
+    SET qtd_estoque = qtd_estoque - OLD.qtd_atual + NEW.qtd_atual
+    WHERE Cod = NEW.Cod_Prod;
+END //
+
+DELIMITER ;
+
+#Trigger para deletar 
+
+DELIMITER //
+
+CREATE TRIGGER atualizar_estoque_delete
+AFTER DELETE ON ITEM_ESTOQUE
+FOR EACH ROW
+BEGIN
+    UPDATE PRODUTO_REF
+    SET qtd_estoque = qtd_estoque - OLD.qtd_atual
+    WHERE Cod = OLD.Cod_Prod;
+END //
+
+DELIMITER ;
+## Falta testar
+
+
+
+#trigger do para qauntidade de funcionarios na filial
+
+
+###Pode usar como teste
+#INSERT INTO FUNCIONARIO (CPF, id_Filial, Data_admissao, Sexo, Estado_Civil, Login, Senha, RG, Nome, Situação, Endereco)
+#VALUES ('12345678902', 1, '2023-10-01', 'M', 'Solteiro', 'joao.silva', 'senha123', '1234567890', 'João Silva', TRUE, 'Rua A, 123');
+
+
+#DELETE FROM FUNCIONARIO WHERE CPF = '12345678902';
+
+# Verificar se a quantidade de funcionários na filial foi atualizada
+#SELECT * FROM FILIAL WHERE ID = 1;
+###Pode usar como teste
+
+
+#Inserir
+DELIMITER //
+
+CREATE TRIGGER after_funcionario_insert
+AFTER INSERT ON FUNCIONARIO
+FOR EACH ROW
+BEGIN
+    UPDATE FILIAL
+    SET Qtde_fun = Qtde_fun + 1
+    WHERE ID = NEW.id_Filial;
+END //
+
+DELIMITER ;
+
+
+#Para retirar 
+
+DELIMITER //
+
+CREATE TRIGGER after_funcionario_delete
+AFTER DELETE ON FUNCIONARIO
+FOR EACH ROW
+BEGIN
+
+    UPDATE FILIAL
+    SET Qtde_fun = Qtde_fun - 1
+    WHERE ID = OLD.id_Filial;
+END //
+
+DELIMITER ;
+
 
